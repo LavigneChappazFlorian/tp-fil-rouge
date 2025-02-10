745c8400cd6c734e9a945153f287fe3b17dfc008
Florian Lavigne Chappaz <florian.lavigne.chappaz@gmail.com>
2025-02-10 15:05:48 +0100

    add update announce function

--- project/src/Controller/AnnounceController.php
+++ project/src/Controller/AnnounceController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AnnounceController extends AbstractController
 {
-    #[Route('/announces', name: 'app_announce')]
+    #[Route('/announces', name: 'announce')]
     public function index(AnnounceRepository $announceRepository): Response
     {
         $announces = $announceRepository->findAll();
@@ -23,7 +23,7 @@ final class AnnounceController extends AbstractController
         ]);
     }
 
-    #[Route('/announces/create', name: 'app_announce_create')]
+    #[Route('/announces/create', name: 'announce_create')]
     public function new(Request $request, EntityManagerInterface $em): Response
     {
         $announce = new Announce();
@@ -35,10 +35,31 @@ final class AnnounceController extends AbstractController
             $em->persist($announce); // prépare/mets en attente les données avant de les insérer ou update dans la bdd
             $em->flush(); // éxécute réellement les requêtes SQL en attente
 
-            return $this->redirectToRoute('app_announce');
+            return $this->redirectToRoute('announce');
         }
 
         return $this->render('announce/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/announces/{id}/edit', name: 'announce_edit', requirements: ['id' => '\d+'])]
+    public function edit(AnnounceRepository $announceRepository, Request $request, EntityManagerInterface $em, int $id): Response
+    {
+        $one_announce = $announceRepository->find($id);
+
+        $form = $this->createForm(CreateAnnounceType::class, $one_announce); // création d'un formulaire avec la récupération du formulaire "type"
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($one_announce); // prépare/mets en attente les données avant de les insérer ou update dans la bdd
+            $em->flush(); // éxécute réellement les requêtes SQL en attente
+
+            return $this->redirectToRoute('announce');
+        }
+
+        return $this->render('announce/edit.html.twig', [
+            'one_announce' => $one_announce,
             'form' => $form->createView(),
         ]);
     }
